0c02ae93832f81f4c5d3471a568d2c9213fe1cc5
jimmyhlee94 <jimmyhlee94@gmail.com>
2015-07-13 21:05:42 -0500

    models project stub

---
+++ RoomMe.Webservice/RoomMe.Webservice.Models/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomMe.Webservice.Models
+{
+    public class Class1
+    {
+    }
+}
