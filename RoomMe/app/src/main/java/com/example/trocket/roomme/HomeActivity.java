0a98448c4249a1b0c1af8c6b71000e56ff6e8552
Zach Plata <zach.plata@cerner.com>
2015-07-15 10:46:30 -0500

    Adding of navigation drawer

--- RoomMe/app/src/main/java/com/example/trocket/roomme/HomeActivity.java
+++ RoomMe/app/src/main/java/com/example/trocket/roomme/HomeActivity.java
@@ -1,13 +1,18 @@
 package com.example.trocket.roomme;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -17,7 +22,16 @@ public class HomeActivity extends ActionBarActivity {
     private ArrayList<User> userList = new ArrayList<User>();
     private UserArrayAdapter adapter;
 
-    public ListView list;
+    private ListView list;
+
+    // Nav Drawer Views
+    private DrawerLayout nav_drawer_layout;
+    private ListView nav_list;
+    private ActionBarDrawerToggle nav_drawer_toggle;
+    private ArrayAdapter<String> nav_adapter;
+    private String nav_title;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,13 +55,143 @@ public class HomeActivity extends ActionBarActivity {
                 setContentView(R.layout.activity_profile_view);
             }
         });
+
+        nav_drawer_layout = (DrawerLayout) findViewById(R.id.ah_drawer_layout);
+        nav_list = (ListView) findViewById(R.id.ah_nav_list);
+        nav_title = getTitle().toString();
+
+        addDrawerItems();
+        setupDrawer();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        /*nav_drawer_layout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        nav_list.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, nav_actions));
+        nav_list.setOnItemClickListener(new DrawerItemClickListener());
+
+        nav_drawer_toggle = new ActionBarDrawerToggle(this, nav_drawer_layout, R.string.drawer_open, R.string.drawer_close) {
+            public void onDrawerClosed(View view) {
+               // getSupportActionBar().setTitle(nav_title);
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                //getSupportActionBar().setTitle(nav_title);
+                invalidateOptionsMenu();
+            }
+        };
+        nav_drawer_layout.setDrawerListener(nav_drawer_toggle);
+
+        if(savedInstanceState == null) {
+            selectItem(0);
+        }*/
+
+    }
+
+    private void addDrawerItems() {
+        String[] osArray = { "Edit Profile", "RoomMe List", "Poop", "Settings", "Cocoa" };
+        nav_adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
+        nav_list.setAdapter(nav_adapter);
+
+        nav_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(HomeActivity.this, "Time for an upgrade!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void setupDrawer() {
+        nav_drawer_toggle = new ActionBarDrawerToggle(this, nav_drawer_layout, R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Navigation!");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(nav_title);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        nav_drawer_toggle.setDrawerIndicatorEnabled(true);
+        nav_drawer_layout.setDrawerListener(nav_drawer_toggle);
+    }
+
+    /*private class DrawerItemClickListener implements ListView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    private void selectItem(int position) {
+        Fragment fragment = new ActionFragment();
+        Bundle args = new Bundle();
+        args.putInt(ActionFragment.ARG_ACTION_NUMBER, position);
+        fragment.setArguments(args);
+
+        FragmentManager fragMan = getFragmentManager();
+        fragMan.beginTransaction().replace(R.id.ah_content_frame, fragment).commit();
+
+        nav_list.setItemChecked(position, true);
+        //setTitle(nav_actions[position]);
+        nav_drawer_layout.closeDrawer(nav_list);
+    }*/
+
+    /**@Override
+    public void setTitle(CharSequence title) {
+        nav_title = title;
+        getSupportActionBar().setTitle(nav_title);
+    }*/
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        nav_drawer_toggle.syncState();
     }
 
     @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        nav_drawer_toggle.onConfigurationChanged(newConfig);
+    }
+
+    /*public static class ActionFragment extends Fragment {
+        public static final String ARG_ACTION_NUMBER = "action_number";
+
+        public ActionFragment() {}
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.drawer_fragment_edit, container, false);
+            int i = getArguments().getInt(ARG_ACTION_NUMBER);
+            String action = getResources().getStringArray(R.array.actions_array)[i];
+
+            ((ImageView) rootView.findViewById(R.id.dfe_pic)).setImageResource(R.drawable.isu);
+            getActivity().setTitle(action);
+            return rootView;
+        }
+    }*/
+
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_home, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        boolean drawerOpen = nav_drawer_layout.isDrawerOpen(nav_list);
+        menu.findItem(R.id.action_settings).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
     }
 
     @Override
@@ -55,6 +199,10 @@ public class HomeActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+
+        if(nav_drawer_toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
--- RoomMe/app/src/main/java/com/example/trocket/roomme/ProfileViewActivity.java
+++ RoomMe/app/src/main/java/com/example/trocket/roomme/ProfileViewActivity.java
@@ -2,17 +2,22 @@ package com.example.trocket.roomme;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class ProfileViewActivity extends ActionBarActivity {
 
+    private com.beardedhen.androidbootstrap.BootstrapCircleThumbnail pic;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_view);
+
+        pic = (com.beardedhen.androidbootstrap.BootstrapCircleThumbnail) findViewById(R.id.pv_user_picture);
+
+        pic.setImage(R.drawable.isu);
     }
 
     @Override
@@ -35,5 +40,18 @@ public class ProfileViewActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if((keyCode == KeyEvent.KEYCODE_BACK)) {
+            onBackPressed();
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 }
