cb4ce9d543cfc76522ce8a68710c9a9edb93e45b
jimmyhlee94 <jimmyhlee94@gmail.com>
2015-07-18 02:23:55 -0500

    userDAO

---
+++ RoomMe.Webservice/RoomMe.Webservice/DataAccess/DAO/UserDAO.cs
@@ -0,0 +1,74 @@
+﻿using RoomMe.Webservice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RoomMe.Webservice.DataAccess.DAO
+{
+    public class UserDAO
+    {
+        private RoomMeWebserviceContext _context = null;
+        public UserDAO(RoomMeWebserviceContext context)
+        {
+            _context = context;
+        }
+
+        public List<User> GetUsersByTagID(int tagID)
+        {
+            using(var db = new RoomMeWebserviceContext())
+            {
+                var tag = db.Tags.Find(tagID);
+                if(tag != null)
+                {
+                    return tag.Users;
+                }
+                return null;
+            }
+        }
+
+        public List<User> GetUsersByTagIDs(int[] tagIDs)
+        {
+            var users = new List<User>();
+            foreach(var id in tagIDs)
+            {
+                var returnedUsers = GetUsersByTagID(id);
+                foreach(var user in returnedUsers)
+                {
+                    if(!users.Contains(user))
+                    {
+                        users.Add(user);
+                    }
+                }
+            }
+
+            return users;
+        }
+
+        public List<User> GetUsersByHousingID(int housingID)
+        {
+            using(var db = new RoomMeWebserviceContext())
+            {
+                var housing = db.Housings.Find(housingID);
+                if(housing != null)
+                {
+                    return housing.Residents;
+                }
+                return null;   
+            }
+        }
+
+        public List<User> GetUsersByCareerID(int careerID)
+        {
+            using(var db = new RoomMeWebserviceContext())
+            {
+                var career = db.Careers.Find(careerID);
+                if(career != null)
+                {
+                    return career.Users;
+                }
+                return null;
+            }
+        }
+    }
+}
