96f422e2d473ccff4e8c65b04e9bc869ee26d208
jimmyhlee94 <jimmyhlee94@gmail.com>
2015-07-18 02:13:45 -0500

    preferencesDAO

---
+++ RoomMe.Webservice/RoomMe.Webservice/DataAccess/DAO/PreferencesDAO.cs
@@ -0,0 +1,30 @@
+﻿using RoomMe.Webservice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RoomMe.Webservice.DataAccess.DAO
+{
+    public class PreferencesDAO
+    {
+        private RoomMeWebserviceContext _context = null;
+        public PreferencesDAO(RoomMeWebserviceContext context)
+        {
+            _context = context;
+        }
+
+        public Preferences GetPreferencesByUserID(int userID)
+        {
+            using(var db = new RoomMeWebserviceContext())
+            {
+                var user = db.Users.Find(userID);
+                if(user != null)
+                {
+                    return user.Preferences;
+                }
+                return null;
+            }
+        }
+    }
+}
