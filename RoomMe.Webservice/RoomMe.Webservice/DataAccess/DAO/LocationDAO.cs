e89ffb51bd1fc5112accd24cda2f714c7902a206
jimmyhlee94 <jimmyhlee94@gmail.com>
2015-07-18 02:26:50 -0500

    locationDAO

---
+++ RoomMe.Webservice/RoomMe.Webservice/DataAccess/DAO/LocationDAO.cs
@@ -0,0 +1,43 @@
+﻿using RoomMe.Webservice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RoomMe.Webservice.DataAccess.DAO
+{
+    public class LocationDAO
+    {
+        private RoomMeWebserviceContext _context = null;
+        public LocationDAO(RoomMeWebserviceContext context)
+        {
+            _context = context;
+        }
+
+        public List<Location> GetLocationsByPreferenceID(int preferenceID)
+        {
+            using (var db = new RoomMeWebserviceContext())
+            {
+                var pref = db.Preferences.Find(preferenceID);
+                if(pref != null)
+                {
+                    return pref.Locations;
+                }
+                return null;
+            }
+        }
+
+        public Location GetLocationByHousingID(int housingID)
+        {
+            using (var db = new RoomMeWebserviceContext())
+            {
+                var housing = db.Housings.Find(housingID);
+                if(housing != null)
+                {
+                    return housing.Location;
+                }
+                return null;
+            }
+        }
+    }
+}
