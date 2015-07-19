51c1f9e1c48c6dc99f5c73471a521948447b80f4
Zach Plata <zach.plata@cerner.com>
2015-07-19 17:41:07 -0500

    New profile edit view

--- RoomMe/app/src/main/java/com/example/trocket/roomme/ProfileEditFragment.java
+++ RoomMe/app/src/main/java/com/example/trocket/roomme/ProfileEditFragment.java
@@ -5,11 +5,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 
 public class ProfileEditFragment extends Fragment {
 
+    private Spinner status;
+    private int statusPosition;
 
+    String[] items = {
+            "Has vacancy",
+            "Needs housing and roommate",
+            "Only need roommate(s)",
+            "Inactive"
+    };
 
     public ProfileEditFragment() {
 
@@ -21,7 +33,34 @@ public class ProfileEditFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_profile_edit, container, false);
+        status = (Spinner) rootView.findViewById(R.id.fpe_edit_status);
+
+
+        /*ArrayAdapter<CharSequence> staticAdapter = ArrayAdapter.createFromResource(getActivity().getApplicationContext(),
+                R.array.status_options, android.R.layout.simple_spinner_item);
+        status.setAdapter(staticAdapter);*/
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(),
+                android.R.layout.simple_spinner_item, items);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        status.setAdapter(adapter);
+        status.setSelection(0);
+        status.setOnItemSelectedListener(new SpinnerItemSelected());
+
         return rootView;
+    }
+
+    private class SpinnerItemSelected implements AdapterView.OnItemSelectedListener {
+
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            statusPosition = position;
+            Toast.makeText(getActivity().getApplicationContext(), items[position], Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
     }
 
 
